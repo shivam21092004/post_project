b4fecc7b5e8a9ffe0c6ab380cb94fa19a56b53dd
Shivam Bartwal <152388070+shivam21092004@users.noreply.github.com>
2025-10-15 13:16:22 +0530

    Update PostList.jsx

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,12 @@
-// src/components/PostList.jsx
+
 import React, { useEffect, useState } from "react";
 import PostItem from "./PostItem";
 import Loader from "./Loader";
 import ErrorMessage from "./ErrorMessage";
-
 const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => {
@@ -19,10 +17,8 @@ const PostList = () => {
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
   }, []);
-
   if (loading) return <Loader />;
   if (error) return <ErrorMessage message={error} />;
-
   return (
     <div>
       {posts.map((post) => (
